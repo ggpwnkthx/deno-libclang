ca75750802e5616ecf4792da66ba5d86eab9222d
Isaac Jessup <ibjessup@gmail.com>
2026-02-24 21:25:31 +0000

    export utils

--- mod.ts
+++ mod.ts
@@ -4,8 +4,7 @@
  * FFI bindings for libclang - the C API for the Clang compiler
  */
 
-// Re-export high-level API
 export * from "./src/libclang.ts";
-
-// Re-export FFI types
 export * from "./src/ffi/types.ts";
+export * from "./src/utils/ffi.ts";
+export * from "./src/utils/platform.ts";
